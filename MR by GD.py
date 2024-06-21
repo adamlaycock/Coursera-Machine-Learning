7f36b34e75a2640af5f254e800708030fa26435c
adam <adam.m.laycock@gmail.com>
2024-06-21 15:46:59 +0100

    changed iteration number

--- MR by GD.py
+++ MR by GD.py
@@ -46,4 +46,4 @@ def gradient_descent(df,
 
 list=['Feature_1', 'Feature_2', 'Feature_3']
 w_1 = np.array([0, 0, 0])
-gradient_descent(ts_df, list, 'Target', w_1, 0, 0.001, 1e-6, 10000)
+gradient_descent(ts_df, list, 'Target', w_1, 0, 0.001, 1e-6, 100000)
